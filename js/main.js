c014b9183997b0c759ce1617843e235b1a649b35
Ari-Ario <xusrew.z@gmail.com>
2025-01-14 17:00:32 +0100

    separated header and footer

--- js/main.js
+++ js/main.js
@@ -26,8 +26,35 @@
 =========================================
 [End Activation Code]
 =========================================*/ 
+
+const loadHeader = () => {
+	fetch('./components/header.html')
+	.then(res => {
+		return res.text()
+	})
+	.then(data => {
+		document.querySelector('#header__part').innerHTML = data;
+	})
+	console.log('header');
+}
+
+const loadFooter = () => {
+    fetch('./components/footer.html')
+    .then(res => {
+        return res.text();
+    })
+    .then(data => {
+        document.querySelector('#footer__part').innerHTML = data ; 
+    })
+    console.log('footer');
+}
+
+loadHeader();
+loadFooter();
+
 (function($) {
     "use strict";
+
      $(document).on('ready', function() {
 	
         jQuery(window).on('scroll', function() {
